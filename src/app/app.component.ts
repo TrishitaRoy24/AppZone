8cf5aac2471ee23f23f420bc740e412d03341032
TrishitaRoy24 <roytrishita24@gmail.com>
2024-11-16 21:03:33 +0530

    basic structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { LeftSideBarComponent } from './Components/left-side-bar/left-side-bar.component';
+import { MainBodyComponent } from './Components/main-body/main-body.component';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [LeftSideBarComponent, MainBodyComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
