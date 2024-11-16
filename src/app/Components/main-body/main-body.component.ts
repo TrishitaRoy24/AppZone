718763f2ed99d43f95e125822b31df186b20330d
TrishitaRoy24 <roytrishita24@gmail.com>
2024-11-16 21:36:32 +0530

    angular material successfully installed

--- src/app/Components/main-body/main-body.component.ts
+++ src/app/Components/main-body/main-body.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
 @Component({
   selector: 'app-main-body',
   standalone: true,
-  imports: [],
+  imports: [MatSlideToggleModule],
   templateUrl: './main-body.component.html',
   styleUrl: './main-body.component.scss'
 })
