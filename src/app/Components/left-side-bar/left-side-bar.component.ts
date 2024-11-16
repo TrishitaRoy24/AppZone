a3b40f75547e61c5ccb9ca949b45c0b6638b85a8
TrishitaRoy24 <roytrishita24@gmail.com>
2024-11-16 23:04:41 +0530

    menu & icons

--- src/app/Components/left-side-bar/left-side-bar.component.ts
+++ src/app/Components/left-side-bar/left-side-bar.component.ts
@@ -1,13 +1,31 @@
 import { Component } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-left-side-bar',
   standalone: true,
-  imports: [MatIconModule],
+  imports: [MatIconModule, RouterModule],
   templateUrl: './left-side-bar.component.html',
   styleUrl: './left-side-bar.component.scss'
 })
 export class LeftSideBarComponent {
 
+  menuItems: any[] = [
+    {
+      link: 'dashboard',
+      lable: 'Dashboard',
+      icon: 'dashboard'
+    },
+    {
+      link: 'products',
+      lable: 'Products',
+      icon: 'shopping_cart'
+    },
+    {
+      link: 'settings',
+      lable: 'Settings',
+      icon: 'setting'
+    }
+  ]
 }
