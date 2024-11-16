f88c79dd044fd814e86842049da63e2003943be4
TrishitaRoy24 <roytrishita24@gmail.com>
2024-11-16 21:48:27 +0530

    changes

--- src/app/Components/left-side-bar/left-side-bar.component.ts
+++ src/app/Components/left-side-bar/left-side-bar.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-left-side-bar',
   standalone: true,
-  imports: [],
+  imports: [MatIconModule],
   templateUrl: './left-side-bar.component.html',
   styleUrl: './left-side-bar.component.scss'
 })
