13e1eabd4f80e9725c55c40ecf4356e201497a76
TrishitaRoy24 <roytrishita24@gmail.com>
2024-11-17 22:09:18 +0530

    route done

--- src/app/Components/main-body/main-body.component.ts
+++ src/app/Components/main-body/main-body.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-main-body',
   standalone: true,
-  imports: [MatSlideToggleModule],
+  imports: [MatSlideToggleModule, RouterOutlet],
   templateUrl: './main-body.component.html',
   styleUrl: './main-body.component.scss'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { ProductsComponent } from './Components/products/products.component';
+import { DashboardComponent } from './Components/dashboard/dashboard.component';
+import { SettingsComponent } from './Components/settings/settings.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path:'',redirectTo: 'dashboard', pathMatch: 'full'},
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'products', component: ProductsComponent },
+  {path:'settings',component:SettingsComponent}
+];
